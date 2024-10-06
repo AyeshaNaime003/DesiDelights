41dd676f3075ac739c8b59e691654b8ef14a91e5
AyeshaNaime003 <ayeshanaime003@gmail.com>
2024-10-07 02:01:17 +0300

    one result->one_or_none

--- server/db_utils.py
+++ server/db_utils.py
@@ -16,16 +16,17 @@ async def get_order_details(order_id, session: AsyncSession):
         print(f"An error occurred: {e}")
         return None  
 
+
 async def get_order_status(order_id, session: AsyncSession):
     try:
         # Create the query and execute
         query = select(Order.status).where(Order.order_id == order_id)
         result = await session.execute(query)
-        order_status = result.scalars().first()
-        if order_status is not None:
-            return str(order_status)  # Directly return the order status
-        else:
-            return None  # Handle case where order_id does not exist
+        order_status = result.scalars().one_or_none()
+        return str(order_status) if order_status else None
     except Exception as e:
         print(f"An error occurred: {e}")
-        return None  
+        return None 
+    
+def get_menu_items(session: AsyncSession):
+    pass
