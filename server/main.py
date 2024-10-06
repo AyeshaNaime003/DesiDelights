2ed29babfd852862ae19c9a3ce9f14494e49b130
AyeshaNaime003 <ayeshanaime003@gmail.com>
2024-10-07 02:01:49 +0300

    ongoing_orders, add item, inetent handlers

--- server/main.py
+++ server/main.py
@@ -6,6 +6,51 @@ from database import get_db
 from model import OrderDetails
 import db_utils
 
+ongoing_orders = {}
+
+def format_order(order_dictionary):
+    formatted_order = []
+    for i, q in order_dictionary.items():
+        formatted_order.append(f"{int(q)} {i}")
+    formatted_order = ', '.join(formatted_order)
+    return formatted_order
+        
+
+async def ongoing_tracking_provide_id(parameters: dict, session: AsyncSession):
+    order_id = int(parameters["number"])
+    order_status = await db_utils.get_order_status(order_id, session)
+    return f"Order {order_id} is {order_status}" if order_status else "Sorry, couldn't find any details for your order."
+
+async def ongoing_order_add(parameters: dict, session: AsyncSession):
+    menu_items = parameters["menu-items"]
+    quantities = parameters["number"]
+    
+    if len(menu_items)!=len(quantities):
+        return "Please specify quanity for each item" 
+    else: 
+        session_id = parameters["session_id"].split('/')[-1]
+        current_session_order = ongoing_orders.get(session_id, {})
+        if len(current_session_order)==0:
+            print('new session id ')
+        else:
+            print('existing session id, adding new items')
+        for quantity,menu_item in zip(quantities, menu_items):
+            current_session_order[menu_item] = quantity
+        ongoing_orders[session_id] = current_session_order  
+        print('\n\n'+str(ongoing_orders)+'\n\n')
+        return f"Order uptil now is {format_order(current_session_order)}. Anything else?"
+    
+async def ongoing_order_delete():
+    pass
+async def ongoing_order_finalize():
+    pass
+
+intentHandler = {
+    "ongoing-tracking.provide_id":ongoing_tracking_provide_id,
+    "ongoing-order.add":ongoing_order_add,
+    "ongoing-order.delete":ongoing_order_delete,
+    "ongoing-order.finalize":ongoing_order_finalize,
+}
 
 app = FastAPI()
 
@@ -16,14 +61,14 @@ async def root():
 @app.post("/")
 async def handle_dialogflow_request(request: Request, session: AsyncSession = Depends(get_db)):
     payload = await request.json()
-    print(payload)
+    print('\n\n'+str(payload)+"\n\n")
     intent = payload["queryResult"]["intent"]["displayName"]
     parameters = payload["queryResult"]["parameters"]
-    if intent=="ongoing-tracking.provide_id":
-        order_status = await db_utils.get_order_status(parameters["number"], session)
-        fulfillment_text = f"Your order is {order_status}" if order_status  else  "Sorry, I couldn't find any details for your order."
-        return JSONResponse(content={
-            "fulfillmentText": fulfillment_text,
-        })
+    parameters["session_id"] = payload["session"]
+
+    return JSONResponse(content={"fulfillmentText": await intentHandler[intent](parameters, session)})
+       
 
 
+
+ 
